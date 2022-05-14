0322caa8b5f513b578bf97cb1f36ccfdc01494c3
Mike Haboustak <haboustak@gmail.com>
2022-05-14 09:21:43 -0400

    Update network configuration when renaming a netdev
    A netdev may be using a custom network config, for example to set DHCP
    options, and that network config may match the netdev by name. linkctl's
    rename operation needs to detect this situation and ensure the
    network is still applied after the link is renamed.

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/haboustak/linkctl
 
-go 1.14
+go 1.18
 
 require (
+	github.com/smartystreets/goconvey v1.7.2 // indirect
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae
 	gopkg.in/ini.v1 v1.57.0
 )
--- internal/networkd/unit.go
+++ internal/networkd/unit.go
@@ -1,9 +1,11 @@
 package networkd
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"gopkg.in/ini.v1"
 )
@@ -93,4 +95,143 @@ func NewUnit(path string) (*Unit, error) {
 	unit.File = iniFile
 
 	return &unit, nil
+}
+
+func (self *Unit) NewDropin(name string) (*Unit, error) {
+	dropinPath := fmt.Sprintf(
+		"/etc/systemd/network/%s.d/%s.conf",
+		self.Name,
+		name)
+
+	return NewUnit(dropinPath)
+}
+
+func (self *Unit) Dropins() []string {
+	dropinPath := fmt.Sprintf("/etc/systemd/network/%s.d/*.conf", self.Name)
+	dropins, err := filepath.Glob(dropinPath)
+	if err != nil {
+		panic(fmt.Errorf("Failed to list units at %s: %w", dropinPath, err))
+	}
+
+	return dropins
+}
+
+func (self *Unit) DropinUnits() chan *Unit {
+	r := make(chan *Unit, 5)
+
+	go func() {
+		for _, dropin := range self.Dropins() {
+			unit, err := NewUnit(dropin)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "WARN: Failed to parse networkd unit %s: %v\n", dropin, err)
+				continue
+			}
+			r <- unit
+		}
+		close(r)
+	}()
+
+	return r
+}
+
+func (self *Unit) ContainsValue(section string, key string, value string) bool {
+	for _, v := range self.GetValues(section, key) {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (self *Unit) Set(section string, key string, value string) error {
+	if value == "" {
+		return self.Remove(section, key)
+	}
+
+	s := self.File.Section(section)
+	s.NewKey(key, value)
+	return self.Save()
+}
+
+func (self *Unit) Get(section string, key string) string {
+	s := self.File.Section(section)
+	if s == nil || !s.HasKey(key) {
+		return ""
+	}
+
+	return s.Key(key).String()
+}
+
+func (self *Unit) GetValues(section string, key string) []string {
+	if value := self.Get(section, key); value != "" {
+		return strings.Split(self.Get(section, key), " ")
+	}
+	return []string{}
+}
+
+func (self *Unit) SetValues(section string, key string, values []string) error {
+	return self.Set(section, key, strings.Join(values, " "))
+}
+
+func (self *Unit) Remove(section string, key string) error {
+	s := self.File.Section(section)
+	if s == nil {
+		return nil
+	}
+
+	s.DeleteKey(key)
+
+	// Delete the file if no more config exists
+	if self.IsEmpty() {
+		if err := self.Delete(); err != nil {
+			return err
+		}
+	} else if err := self.Save(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (self *Unit) Replace(section string, key string, old string, value string) error {
+	values := self.GetValues(section, key)
+	fmt.Printf("Setting value from %v\n", values)
+	update := make([]string, len(values)+1)
+	kept := 0
+	for _, v := range values {
+		if v != old && v != value {
+			update[kept] = v
+			kept += 1
+		}
+	}
+	fmt.Printf("Setting value after %v with |%s|\n", update, value)
+	update[kept] = value
+	fmt.Printf("Setting value to %v\n", update)
+	return self.SetValues(section, key, update)
+}
+
+func (self *Unit) Include(section string, key string, value string) error {
+	values := self.GetValues(section, key)
+	for _, v := range values {
+		if v == value {
+			return nil
+		}
+	}
+	values = append(values, value)
+	return self.SetValues(section, key, values)
+}
+
+func (self *Unit) Exclude(section string, key string, value string) error {
+	values := self.GetValues(section, key)
+	update := make([]string, len(values))
+	kept := 0
+	for _, v := range values {
+		if v != value {
+			update[kept] = v
+			kept += 1
+		}
+	}
+
+	return self.SetValues(section, key, update)
 }
